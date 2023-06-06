f6ce081e71c46d873b7a61b4574ed39250566c22
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-06-06 03:20:45 +0300

    Update qr.py

--- qr.py
+++ qr.py
@@ -42,7 +42,7 @@ def qr_householder(A):
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
-        for _ in range(n - 1):
+        for _ in range(50000):
             s = Ak.item(n - 1, n - 1)
             smult = s * np.eye(n, dtype=np.complex128)
             futures.append(executor.submit(householder, Ak - smult))
@@ -59,7 +59,7 @@ def qr_gram_schmidt(A):
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
-        for _ in range(n - 1):
+        for _ in range(50000):
             s = Ak[n - 1, n - 1]
             smult = s * np.eye(n, dtype=np.complex128)
             futures.append(executor.submit(gram_schmidt, Ak - smult))
